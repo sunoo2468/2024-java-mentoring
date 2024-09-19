db598941342396f70480261005d37c20be90ab3c
sunoo2468@gmail.com <sunoo2468@naver.com>
2024-09-20 03:56:32 +0900

    Refactor: 리드미 삭제

--- mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
@@ -29,3 +29,4 @@ public class CalculatorController {
         outputview.outputResult(divisionNumber);
     }
 }
+
--- mission/mission1-calculator/src/main/java/com/calculator/view/OutputView.java
+++ mission/mission1-calculator/src/main/java/com/calculator/view/OutputView.java
@@ -6,3 +6,4 @@ public class OutputView {
         System.out.println("Result: "+ resultNumber );
     }
 }
+
